9a871b5e134e3152c53155e361b903316e9560b1
gulcinisgenderova <mi7rtfggj@code.edu.az>
2024-05-02 19:41:41 +0400

    sdsjaad

---
+++ adminpanel/adminpanel/src/pages/Admin/index.jsx
@@ -0,0 +1,13 @@
+
+const Admin = ({suppliers}) => {
+  console.log(suppliers);
+  return (
+    <ul>
+      {/* {suppliers?.map((elem) => {
+        return <li>{elem.id}</li>;
+      })} */}
+    </ul>
+  );
+};
+
+export default Admin;
